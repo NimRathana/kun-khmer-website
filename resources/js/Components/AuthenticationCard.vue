e969d7eb7a035f93c0ef73b5ee49523ed615f350
Nim Rathana <126838139+NimRathana@users.noreply.github.com>
2025-01-22 10:14:58 +0700

    commit

--- resources/js/Components/AuthenticationCard.vue
+++ resources/js/Components/AuthenticationCard.vue
@@ -10,43 +10,37 @@
             </div>
         </v-card>
     </div> -->
-    <v-container
-        class="min-h-screen flex flex-col sm:justify-center items-center pa-5"
-    >
-        <v-row justify="center" class="d-flex align-center" style="max-width: calc(100%);min-width: calc(30%);">
-            <v-col cols="12" class="pa-0 ma-0" style="max-width: calc(100%);min-width: calc(90%);">
-                <v-card elevation="10" :variant="colorStore.selectedSkin == 'bordered' ? 'outlined' : 'elevated'">
-                    <v-row justify="center" class="pt-5">
-                        <div class="d-flex justify-end" style="position: absolute;right:5px;top:10px;">
-                            <v-menu offset-y>
-                                <template #activator="{ props }">
-                                    <v-btn v-bind="props" icon variant="text">
-                                        <v-icon>
-                                        {{ colorStore.theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night' }}
-                                        </v-icon>
-                                    </v-btn>
-                                </template>
-                                <v-list>
-                                    <v-list-item @click="ChangeTheme('light')">
-                                        <v-list-item-title><v-icon>mdi-weather-sunny</v-icon> {{ $t('global.light_theme') }}</v-list-item-title>
-                                    </v-list-item>
-                                    <v-list-item @click="ChangeTheme('dark')">
-                                        <v-list-item-title><v-icon>mdi-weather-night</v-icon> {{ $t('global.dark_theme') }}</v-list-item-title>
-                                    </v-list-item>
-                                </v-list>
-                            </v-menu>
-                        </div>
-                        <v-col cols="auto">
-                            <slot name="logo" />
-                        </v-col>
-                    </v-row>
+    <v-container class="min-h-screen flex items-center justify-center">
+        <v-card elevation="10" :variant="colorStore.selectedSkin == 'bordered' ? 'outlined' : 'elevated'" style="width: 100%; max-width: 500px;">
+            <v-row justify="center" class="pt-5">
+                <div class="d-flex justify-end" style="position: absolute;right:5px;top:10px;">
+                    <v-menu offset-y>
+                        <template #activator="{ props }">
+                            <v-btn v-bind="props" icon variant="text">
+                                <v-icon>
+                                {{ colorStore.theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night' }}
+                                </v-icon>
+                            </v-btn>
+                        </template>
+                        <v-list>
+                            <v-list-item @click="ChangeTheme('light')">
+                                <v-list-item-title><v-icon>mdi-weather-sunny</v-icon> {{ $t('global.light_theme') }}</v-list-item-title>
+                            </v-list-item>
+                            <v-list-item @click="ChangeTheme('dark')">
+                                <v-list-item-title><v-icon>mdi-weather-night</v-icon> {{ $t('global.dark_theme') }}</v-list-item-title>
+                            </v-list-item>
+                        </v-list>
+                    </v-menu>
+                </div>
+                <v-col cols="auto">
+                    <slot name="logo" />
+                </v-col>
+            </v-row>
 
-                    <v-card-text class="px-6 py-4">
-                        <slot />
-                    </v-card-text>
-                </v-card>
-            </v-col>
-        </v-row>
+            <v-card-text class="px-6 py-4">
+                <slot />
+            </v-card-text>
+        </v-card>
     </v-container>
 </template>
 <script setup>
